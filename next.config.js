0358d11ceae0b1ad6c9658dccd8bdecfa67ef6da
Marek8861 <144486893+Marek8861@users.noreply.github.com>
2025-04-28 21:17:47 +0200

    A

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,      // pilnowanie błędów
-    experimental: {
-      serverActions: true       // WŁĄCZ koniecznie Server Actions jeśli używasz Next 14!
-    },
-    images: {
-      domains: ['your-supabase-domain.supabase.co'] // jeśli chcesz ładować avatary itp.
-    }
-  }
+    reactStrictMode: true,
+  };
   
 module.exports = nextConfig;
-  
